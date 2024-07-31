e5a661e0a3dea93f79834728afb99149e66484a8
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-31 17:19:58 +0700

    first commit

--- components/templates/OnboardingTemplate.tsx
+++ components/templates/OnboardingTemplate.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const OnboardingTemplate = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>OnboardingTemplate</Text>
     </View>
   )
 }
 
-export default index
+export default OnboardingTemplate
 
 const styles = StyleSheet.create({})
