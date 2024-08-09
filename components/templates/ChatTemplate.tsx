5eaa8f1f528d1cf9e3effe3751a63ea2d8eccb8a
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-09 16:25:36 +0700

    implement ChatTemplate

--- components/templates/ChatTemplate.tsx
+++ components/templates/ChatTemplate.tsx
@@ -1,30 +1,75 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import ContainerView from '@/components/atoms/ContainerView'
 import { Header } from '@/components/molecules/Header'
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { dataUser } from '@/constants/DataUser';
+import { ItemListUser } from '../molecules/ItemListUser';
+import dataContact from '@/constants/DataContact';
+import { ItemMess } from '../origanisms/ItemMess';
 
 const ChatTemplate = () => {
   return (
-    <ContainerView>
-      <Header
-        title='Chat'
-        iconLeft={
-          <AntDesign name="search1" size={24} color="black" />
-        }
-        iconRight={
-          <Image style={styles.img} source={require('@/assets/images/avt2.png')}/>
-        } />
-        
-    </ContainerView>
+    <ScrollView>
+      <ContainerView>
+        <Header
+          title='Chat'
+          iconLeft={
+            <AntDesign name="search1" size={24} color="black" />
+          }
+          iconRight={
+            <Image style={styles.img} source={require('@/assets/images/avt2.png')} />
+          } />
+
+        <View style={styles.containerUser}>
+          <TouchableOpacity style={{ marginRight: 10 }}>
+            <Image style={styles.imgUser} source={require('@/assets/images/avt2.png')} />
+            <Text style={{ fontSize: 14 }}>My status</Text>
+          </TouchableOpacity>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={dataUser}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) =>
+              <ItemListUser img={item.img} name={item.name} />
+            }
+          />
+        </View>
+
+        <View>
+          {dataContact.map((item) => (
+            <ItemMess
+              imgUser={item.img}
+              messUser={item.note}
+              messedTime='10'
+              nameUser={item.name}
+              quantityMess='3' />
+          ))}
+
+        </View>
+
+      </ContainerView>
+    </ScrollView>
   )
 }
 
 export default ChatTemplate
 
 const styles = StyleSheet.create({
-  img:{
-    height:44,
-    width:44
-  }
+  img: {
+    height: 44,
+    width: 44
+  },
+  containerUser: {
+    flexDirection: "row",
+    marginTop: 40,
+    marginBottom: 30
+  },
+  imgUser: {
+    width: 58,
+    height: 58,
+    marginBottom: 10
+  },
+
 })
