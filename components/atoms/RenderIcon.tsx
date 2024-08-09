c9b45bcde268a0d84dabae47a6c40d7e5460f936
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-09 16:19:35 +0700

    updated component RenderIcon

--- components/atoms/RenderIcon.tsx
+++ components/atoms/RenderIcon.tsx
@@ -1,6 +1,5 @@
-import { isNumber } from "lodash"
 import React from "react"
-import { Image, TouchableOpacity } from "react-native"
+import { TouchableOpacity } from "react-native"
 
 export const RenderIcon = (icon:number | React.ReactElement):React.ReactElement =>{
     return(
