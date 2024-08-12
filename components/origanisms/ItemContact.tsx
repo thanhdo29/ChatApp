3364b3ed338638e90e9d7a5dd3fa35c033fda154
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-12 22:31:32 +0700

    add backIcon for component ItemContact

--- components/origanisms/ItemContact.tsx
+++ components/origanisms/ItemContact.tsx
@@ -2,48 +2,55 @@ import React from "react"
 import { Image, StyleSheet, View } from "react-native"
 import { ContentContact } from "../molecules/ContentContact"
 import { RenderIcon } from "../atoms/RenderIcon"
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { isNil } from "lodash"
 type Props = {
-    namePerson:string,
-    notePerson?:string,
-    img:number | string
-    iconPhone?: string| React.ReactElement,
-    iconVideo?:string| React.ReactElement
+    backIcon?: React.ReactElement
+    namePerson: string,
+    notePerson?: string,
+    img: number | string
+    iconPhone?: string | React.ReactElement,
+    iconVideo?: string | React.ReactElement
 }
-export const ItemContact = (props:Props):React.ReactElement=>{
-    const soureImg= typeof props.img === 'number' ? props.img : {uri:''} 
-    return(
+export const ItemContact = (props: Props): React.ReactElement => {
+    const soureImg = typeof props.img === 'number' ? props.img : { uri: '' }
+    return (
         <View style={styles.container}>
+            {
+                !isNil(props.backIcon) &&
+                <RenderIcon icon={props.backIcon} />
+            }
             <View style={styles.viewLeft}>
-                <Image style={styles.img} source={soureImg}/>
-                <ContentContact namePerson={props.namePerson} notePerson={props.notePerson}/>
+                <Image style={styles.img} source={soureImg} />
+                <ContentContact namePerson={props.namePerson} notePerson={props.notePerson} />
             </View>
             <View style={styles.viewRight}>
-                {!isNil(props.iconPhone) && <RenderIcon icon={props.iconPhone}/>}
-                {!isNil(props.iconVideo) && <RenderIcon icon={props.iconVideo}/>}
+                {!isNil(props.iconPhone) && <RenderIcon icon={props.iconPhone} />}
+                {!isNil(props.iconVideo) && <RenderIcon icon={props.iconVideo} />}
             </View>
         </View>
     )
 }
-const styles=StyleSheet.create({
-    container:{
-        marginVertical:20,
-        flexDirection:"row",
-        justifyContent:"space-between",
-        alignItems:"center"
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
     },
-    viewLeft:{
-        flexDirection:"row",
-        alignItems:"center"
+    viewLeft: {
+        flexDirection: "row",
+        alignItems: "center"
     },
-    viewRight:{
-        flexDirection:"row",
-        alignItems:"center"
+    viewRight: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap:16
     },
-    img:{
-        height:52,
-        width:52,
-        marginRight:14,
-        resizeMode:"contain"
+    img: {
+        height: 52,
+        width: 52,
+        marginRight: 14,
+        resizeMode: "contain"
     }
 })
