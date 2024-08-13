5a74005307e017c40d7018e152a07f2b4bc04bb5
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-13 23:55:07 +0700

    updated If the user enters content into the EditText, the Send icon will be displayed

--- components/molecules/FooterChat.tsx
+++ components/molecules/FooterChat.tsx
@@ -1,25 +1,35 @@
 import { StyleSheet, TextInput, useColorScheme, View, ViewProps } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Feather from '@expo/vector-icons/Feather';
 import { RenderIcon } from '../atoms/RenderIcon';
 import getColors from '@/constants/Colors';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 type Props = {
 
-}& ViewProps
-const FooterChat = (props:Props) => {
+} & ViewProps
+const FooterChat = (props: Props) => {
     const colors = getColors(useColorScheme());
+    const [message, setMessage] = useState<string>('')
     return (
         <View {...props} style={styles.container}>
             <RenderIcon icon={<Feather name="paperclip" size={24} color="black" />} />
             <TextInput
-                style={[styles.input, {backgroundColor:colors.paleMint}]}
+                style={[styles.input, { backgroundColor: colors.paleMint }]}
                 placeholder="Write your message"
                 placeholderTextColor={colors.neutralGray}
+                onChangeText={(txt) => setMessage(txt)}
             />
-            <View style={styles.iconsContainer}>
-                <RenderIcon icon={<Feather name="camera" size={24} color="black" />}/>
-                <RenderIcon icon={<Feather name="mic" size={24} color="black" />}/>
-            </View>
+            {message ?
+                (<RenderIcon
+                    icon={<MaterialCommunityIcons name="send-circle" size={40} color={colors.royalPurple} />} />
+                ) :
+                (<View style={styles.iconsContainer}>
+                    <RenderIcon icon={<Feather name="camera" size={24} color="black" />} />
+                    <RenderIcon icon={<Feather name="mic" size={24} color="black" />} />
+                </View>)
+            }
+
+
         </View>
     );
 };
@@ -28,12 +38,12 @@ export default FooterChat;
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         paddingVertical: 6,
-        position:"absolute",
-        bottom:30
+        position: "absolute",
+        bottom: 30
     },
     input: {
         flex: 1,
