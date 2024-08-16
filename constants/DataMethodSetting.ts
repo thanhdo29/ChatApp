83a95c65b8725ce1e4a8b662962cfe7675d2c01b
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-16 17:34:51 +0700

    add id for method setting

--- constants/DataMethodSetting.ts
+++ constants/DataMethodSetting.ts
@@ -1,43 +1,49 @@
 import { Icons } from "@/components/atoms/Icons"
 
 interface DataMethodSetting {
+    id:string,
     nameMethod: string
     desMethod?: string
     typeIcon: any
     nameIcon: string
 }
 const dataMethodSetting: DataMethodSetting[] = [
-    {
+    {   id:"1",
         nameMethod: 'Account',
         desMethod: 'Privacy, security, change number',
         typeIcon:Icons.Ionicons,
         nameIcon:"key-outline"
     },
     {
+        id:"2",
         nameMethod: 'Chat',
         desMethod: 'Chat history,theme,wallpapers',
         typeIcon:Icons.Ionicons,
         nameIcon:"chatbubble-ellipses-outline"
     },
     {
+        id:"3",
         nameMethod: 'Notifications',
         desMethod: 'Messages, group and others',
         typeIcon:Icons.Feather,
         nameIcon:"bell"
     },
     {
+        id:"4",
         nameMethod: 'Help',
         desMethod: 'Help center,contact us, privacy policy',
         typeIcon:Icons.Feather,
         nameIcon:"help-circle"
     },
     {
+        id:"5",
         nameMethod: 'Storage and data',
         desMethod: 'Network usage, stogare usage',
         typeIcon:Icons.Ionicons,
         nameIcon:"swap-vertical"
     },
     {
+        id:"6",
         nameMethod: 'Log out',
         typeIcon:Icons.MaterialIcons,
         nameIcon:"logout"
