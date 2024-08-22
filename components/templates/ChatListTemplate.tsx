c521fe85b926499019a6f8e9736b6b4e4bc74f19
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-22 16:20:17 +0700

    get list user from Firebase

--- components/templates/ChatListTemplate.tsx
+++ components/templates/ChatListTemplate.tsx
@@ -1,60 +1,99 @@
 import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContainerView from '@/components/atoms/ContainerView'
 import { Header } from '@/components/molecules/Header'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { dataUser } from '@/constants/DataUser';
 import { ItemListUser } from '../molecules/ItemListUser';
-import dataContact from '@/constants/DataContact';
 import { ItemMess } from '../origanisms/ItemMess';
 import { useRouter } from 'expo-router';
+import useStorage from '@/hooks/useStorage';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '@/firebaseConfig';
 
 const ChatListTemplate = () => {
   const router = useRouter()
-  const moveChatDetail = () => {
-    router.push('StackScreen/MessengerTemplate' as never);
+  const { getObjectItem } = useStorage()
+  const USER_LOGIN_APP = "USER-LOGIN-APP"
+  const [users, setUsers] = useState<User[]>([])
+  const [userLogin, setUserLogin] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
+          const userList: User[] = snapshot.docs.map(doc => ({
+            firebaseId: doc.id,
+            ...(doc.data() as User)
+          }))
+          setUsers(userList)
+        })
+        return () => unsubscribe()
+      } catch (error) {
+        console.error("Error get users: " + error)
+      }
+    }
+
+    const getUserLogin = async () => {
+      const userLogin = await getObjectItem(USER_LOGIN_APP)
+      setUserLogin(userLogin as User)
+    }
+
+    fetchUsers()
+    getUserLogin()
+
+  }, [])
+
+  const moveChatDetail = (user: User) => {
+    router.push({
+      pathname: 'StackScreen/MessengerTemplate' as never,
+      params: { user: JSON.stringify(user) }
+    });
   }
+
+  const listUser = users.filter(user => user.uid != userLogin?.uid)
+
   return (
-      <ContainerView>
-        <Header
-          title='Chat'
-          iconLeft={
-            <AntDesign name="search1" size={24} color="black" />
+    <ContainerView>
+      <Header
+        title='Chat'
+        iconLeft={
+          <AntDesign name="search1" size={24} color="black" />
+        }
+        iconRight={
+          <Image style={styles.img} source={require('@/assets/images/avt2.png')} />
+        } />
+
+      <View style={styles.containerUser}>
+        <TouchableOpacity style={{ marginRight: 10 }}>
+          <Image style={styles.imgUser} source={require('@/assets/images/avt2.png')} />
+          <Text style={{ fontSize: 14 }}>My status</Text>
+        </TouchableOpacity>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={dataUser}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) =>
+            <ItemListUser key={item.id} img={item.img} name={item.name} />
           }
-          iconRight={
-            <Image style={styles.img} source={require('@/assets/images/avt2.png')} />
-          } />
+        />
+      </View>
 
-        <View style={styles.containerUser}>
-          <TouchableOpacity style={{ marginRight: 10 }}>
-            <Image style={styles.imgUser} source={require('@/assets/images/avt2.png')} />
-            <Text style={{ fontSize: 14 }}>My status</Text>
-          </TouchableOpacity>
-          <FlatList
-            showsHorizontalScrollIndicator={false}
-            horizontal
-            data={dataUser}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) =>
-              <ItemListUser img={item.img} name={item.name} />
-            }
-          />
-        </View>
+      <ScrollView>
+        {listUser.map((item) => (
+          <ItemMess
+            messedTime='10'
+            nameUser={item.displayName}
+            messUser='jchsd'
+            imgUser={require('@/assets/images/avt2.png')}
+            quantityMess='3'
+            onPress={() => moveChatDetail(item)} />
+        ))}
 
-        <ScrollView>
-          {dataContact.map((item) => (
-            <ItemMess
-              imgUser={item.img}
-              messUser={item.note}
-              messedTime='10'
-              nameUser={item.name}
-              quantityMess='3'
-              onPress={() => moveChatDetail()} />
-          ))}
+      </ScrollView>
 
-        </ScrollView>
-
-      </ContainerView>
+    </ContainerView>
   )
 }
 
