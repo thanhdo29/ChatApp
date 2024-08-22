c9d8b4a20c1584f65e267d16a263f84ff43406fc
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-22 16:21:11 +0700

    performs message sending funtion

--- app/StackScreen/MessengerTemplate.tsx
+++ app/StackScreen/MessengerTemplate.tsx
@@ -1,57 +1,89 @@
-import { FlatList, StyleSheet, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, TouchableOpacity, View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { ItemContact } from '../../components/origanisms/ItemContact'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import ContainerView from '../../components/atoms/ContainerView';
 import FooterChat from '../../components/molecules/FooterChat';
 import { MessageBubble } from '../../components/molecules/MessBubble';
+import { useLocalSearchParams } from 'expo-router';
+import { addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '@/firebaseConfig';
+import useStorage from '@/hooks/useStorage';
 
 const MessengerTemplate = () => {
-  const messengers = [
-    {
-      id: '1',
-      text: "Alo",
-      isSender: true
-    },
-    {
-      id: '2',
-      text: "Game ko",
-      isSender: true
-    },
-    {
-      id: '3',
-      text: "Có",
-      isSender: false
-    },
-    {
-      id: '4',
-      text: "Vào đi sjhgdgdggggggggggggggggggggggggggggggdddđ",
-      isSender: false
-    },
-  ]
+  const user = useLocalSearchParams()
+  const { getObjectItem } = useStorage()
+  const userMess = user ? JSON.parse(user.user as string) : null;
+  const [messages, setMessages] = useState<Message[]>([])
+  const [userLogin, setUserLogin] = useState<User | null>(null)
+  const USER_LOGIN_APP = "USER-LOGIN-APP"
+  const chatId = [userLogin?.uid, userMess.uid].sort().join('_');
+  const chatId2 = [userMess.uid, userLogin?.uid].sort().join('_');
+
+  useEffect(() => {
+    const messagesRef = collection(db, 'chats', chatId2, 'messages');
+    const q = query(messagesRef, orderBy('createAt', 'asc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const messageList: Message[] = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        text: doc.data().text,
+        isSender: doc.data().userSend.uid == userLogin?.uid,
+        createAt: doc.data().createAt.toDate(),
+        userSend: doc.data().userSend
+      }))
+      setMessages(messageList)
+    })
+
+    return () => unsubscribe()
+  }, [userMess.firebaseId, chatId])
+
+  useEffect(() => {
+    const getUserLogin = async () => {
+      const userLogin = await getObjectItem(USER_LOGIN_APP)
+      setUserLogin(userLogin as User)
+    }
+    getUserLogin()
+
+  }, [])
+
+  const handleSendMessage = async (message: string) => {
+    try {
+      const messageRef = collection(db, "chats", chatId, "messages");
+      await addDoc(messageRef, {
+        text: message,
+        userSend: userLogin,
+        isSender: true,
+        createAt: new Date()
+      });
+    } catch (error) {
+      console.error("Error adding message: ", error);
+    }
+  }
+
   return (
     <ContainerView>
       <View style={styles.container}>
         <ItemContact
           img={require('@/assets/images/avt2.png')}
-          namePerson='Đỗ Tuấn Thành'
+          namePerson={userMess.displayName}
           notePerson='Active now'
           iconPhone={<AntDesign name="phone" size={22} color="black" />}
           iconVideo={<Ionicons name="videocam-outline" size={24} color="black" />}
           backIcon={<AntDesign name="arrowleft" size={24} color="black" />} />
         <FlatList
-          data={messengers}
+          data={messages}
           renderItem={({ item }) => (
             <MessageBubble
+              key={item.id}
               message={item.text}
               isSender={item.isSender}
-              namePerson='Đỗ Tuấn Thành'
+              namePerson={item.userSend.displayName}
               imgPerson={require('@/assets/images/avt2.png')} />
           )}
           keyExtractor={(item) => item.id}
         />
-        <FooterChat />
+        <FooterChat onSendMessage={(mess: string) => handleSendMessage(mess)} />
       </View>
     </ContainerView>
   )
